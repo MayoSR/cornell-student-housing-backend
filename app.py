c859c87150247090c96ab0c02e08150849372bd0
MayoSR <mayankrao16@gmail.com>
2022-09-18 17:07:39 -0400

    Dockerfile plus gunicoron

--- app.py
+++ app.py
@@ -1,15 +1,13 @@
 import os
+from venv import create
 
 from flask import Flask
 from flask_cors import CORS
 import random
 
-# create and configure the app
 app = Flask(__name__)
 CORS(app)
 
 @app.route('/hello')
 def hello():
     return 'Your random ID is ' + str(random.randint(10000000,99999999))
-
-app.run(debug=True,port=3001)
